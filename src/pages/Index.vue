015b0cfcf8bdfc3f4f4f53d6d4b9f6e0ad2e78ee
Genievot <40867747+genievot@users.noreply.github.com>
2020-09-27 23:01:06 +0530

    Sedo Network Frontend
    Basic layout

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <div class="row justify-center q-pb-xl">
+    <img
+      style="width:300px"
+      alt="Sedo-Network logo"
+      src="~assets/sedo-network-cropped.svg"
+    >
+    </div>
+  <div class="row justify-center q-pt-xl my-font">
+    <q-input rounded outlined v-model="text" color="grey" style="width:80vw; max-width: 500px; font-size: 25px">
+         <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
